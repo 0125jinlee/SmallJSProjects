b119aff3df8e140caf0a9380f20712600ecc91d9
Jin Won Lee <0125jinwonlee@gmail.com>
2020-12-23 21:27:13 -0800

    6. Background Image Slider Done

--- src/App.js
+++ src/App.js
@@ -31,10 +31,26 @@ const App = () => {
         }}
       >
         <button className="PrevBtn" onClick={prevButtonHandler}>
-          <i classNam="PrevArrow" />
+          <svg
+            className="PrevArr"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 106.43 147.875"
+            x="0px"
+            y="0px"
+          >
+            <path d="M98.93,72.14,60.72,94.21,22.5,116.27a15,15,0,0,1-22.5-13V15A15,15,0,0,1,15,0,14.82,14.82,0,0,1,22.5,2L60.72,24.1,98.93,46.16A15,15,0,0,1,98.93,72.14Z" />
+          </svg>
         </button>
         <button className="NextBtn" onClick={nextButtonHandler}>
-          <i classNam="NextArrow" />
+          <svg
+            className="NextArr"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 106.43 147.875"
+            x="0px"
+            y="0px"
+          >
+            <path d="M98.93,72.14,60.72,94.21,22.5,116.27a15,15,0,0,1-22.5-13V15A15,15,0,0,1,15,0,14.82,14.82,0,0,1,22.5,2L60.72,24.1,98.93,46.16A15,15,0,0,1,98.93,72.14Z" />
+          </svg>
         </button>
       </div>
     </div>
