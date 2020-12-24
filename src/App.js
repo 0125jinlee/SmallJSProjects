12c169bd1824c3df0f220fb8edb6859c737a5c9c
Jin Won Lee <0125jinwonlee@gmail.com>
2020-12-23 22:44:59 -0800

    7. Testimonials In Progress

--- src/App.js
+++ src/App.js
@@ -1,57 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./App.css";
 
 const App = () => {
-  const [imgCounter, setImgCounter] = useState(0);
-
-  const prevButtonHandler = () => {
-    if (imgCounter > 0) {
-      setImgCounter(imgCounter - 1);
-    } else {
-      setImgCounter(4);
-    }
-  };
-
-  const nextButtonHandler = () => {
-    if (imgCounter < 4) {
-      setImgCounter(imgCounter + 1);
-    } else {
-      setImgCounter(0);
-    }
-  };
-
   return (
     <div className="App">
-      <div
-        className="Slider"
-        style={{
-          background:
-            "url(" + imgCounter + ".jpeg) center/cover fixed no-repeat",
-        }}
-      >
-        <button className="PrevBtn" onClick={prevButtonHandler}>
-          <svg
-            className="PrevArr"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 106.43 147.875"
-            x="0px"
-            y="0px"
-          >
-            <path d="M98.93,72.14,60.72,94.21,22.5,116.27a15,15,0,0,1-22.5-13V15A15,15,0,0,1,15,0,14.82,14.82,0,0,1,22.5,2L60.72,24.1,98.93,46.16A15,15,0,0,1,98.93,72.14Z" />
-          </svg>
-        </button>
-        <button className="NextBtn" onClick={nextButtonHandler}>
-          <svg
-            className="NextArr"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 106.43 147.875"
-            x="0px"
-            y="0px"
-          >
-            <path d="M98.93,72.14,60.72,94.21,22.5,116.27a15,15,0,0,1-22.5-13V15A15,15,0,0,1,15,0,14.82,14.82,0,0,1,22.5,2L60.72,24.1,98.93,46.16A15,15,0,0,1,98.93,72.14Z" />
-          </svg>
-        </button>
+      <div className="Main">
+        <div className="Title">
+          <h4>CLIENT</h4>
+          <h1>TESTIMONIALS</h1>
+        </div>
+        <div className="Testimonials">
+          <button className="leftBtn"></button>
+          <div className="Box">
+            <img
+              src="customer-0.jpg"
+              width="150"
+              id="custoer-img"
+              className="customerImg"
+              alt="customer-0"
+            />
+            <h4>CUSTOMER NAME</h4>
+            <p>Lorem</p>
+          </div>
+          <button className="rightBtn"></button>
+        </div>
       </div>
     </div>
   );
