d9e1e0c9b8b0c03ddf73588f22921348e8e5a909
Jin Won Lee <0125jinwonlee@gmail.com>
2020-12-24 17:33:41 -0800

    7. Testimonials Done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
 const App = () => {
+  const [index, setIndex] = useState(0);
+  const customers = [];
+
+  function Customer(img, name, text) {
+    this.img = img;
+    this.name = name;
+    this.text = text;
+  }
+
+  const createCustomer = (img, name, text) => {
+    let fullImg = `customer-${img}.jpg`;
+    let customer = new Customer(fullImg, name, text);
+
+    customers.push(customer);
+  };
+
+  createCustomer(
+    0,
+    "John",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis neque reprehenderit laborum, corporis explicabo assumenda. Porro impedit consectetur animi, reprehenderit recusandae sapiente at aliquam reiciendis modi ipsam rerum suscipit distinctio?"
+  );
+  createCustomer(
+    1,
+    "Sandy",
+    "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock"
+  );
+  createCustomer(
+    2,
+    "Amy",
+    "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+  );
+  createCustomer(
+    3,
+    "Tyrell",
+    "If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
+  );
+  createCustomer(
+    4,
+    "Wanda",
+    "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  );
+
+  const leftBtnHandler = () => {
+    if (index === 0) {
+      setIndex(customers.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  };
+
+  const rightBtnHandler = () => {
+    if (index === customers.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  };
+
+  console.log(index);
+
   return (
     <div className="App">
       <div className="Main">
@@ -11,19 +71,154 @@ const App = () => {
           <h1>TESTIMONIALS</h1>
         </div>
         <div className="Testimonials">
-          <button className="leftBtn"></button>
           <div className="Box">
             <img
-              src="customer-0.jpg"
-              width="150"
+              className="CustomerImg"
+              src={customers[index].img}
               id="custoer-img"
-              className="customerImg"
-              alt="customer-0"
+              alt={index}
             />
-            <h4>CUSTOMER NAME</h4>
-            <p>Lorem</p>
+            <h4>{customers[index].name}</h4>
+            <span className="ReviewIcons">
+              <span className="StarIcon">
+                <svg
+                  class="svg-inline--fa fa-star fa-w-18"
+                  aria-hidden="true"
+                  data-prefix="fas"
+                  data-icon="star"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 576 512"
+                  data-fa-i2svg=""
+                >
+                  <path
+                    fill="currentColor"
+                    d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+                  ></path>
+                </svg>
+              </span>
+              <span className="StarIcon">
+                <svg
+                  class="svg-inline--fa fa-star fa-w-18"
+                  aria-hidden="true"
+                  data-prefix="fas"
+                  data-icon="star"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 576 512"
+                  data-fa-i2svg=""
+                >
+                  <path
+                    fill="currentColor"
+                    d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+                  ></path>
+                </svg>
+              </span>
+              <span className="StarIcon">
+                <svg
+                  class="svg-inline--fa fa-star fa-w-18"
+                  aria-hidden="true"
+                  data-prefix="fas"
+                  data-icon="star"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 576 512"
+                  data-fa-i2svg=""
+                >
+                  <path
+                    fill="currentColor"
+                    d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+                  ></path>
+                </svg>
+              </span>
+              <span className="StarIcon">
+                <svg
+                  class="svg-inline--fa fa-star fa-w-18"
+                  aria-hidden="true"
+                  data-prefix="fas"
+                  data-icon="star"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 576 512"
+                  data-fa-i2svg=""
+                >
+                  <path
+                    fill="currentColor"
+                    d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+                  ></path>
+                </svg>
+              </span>
+              <span className="StarIcon">
+                <svg
+                  class="svg-inline--fa fa-star-half fa-w-18"
+                  aria-hidden="true"
+                  data-prefix="fas"
+                  data-icon="star-half"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 576 512"
+                  data-fa-i2svg=""
+                >
+                  <path
+                    fill="currentColor"
+                    d="M288 0c-11.4 0-22.8 5.9-28.7 17.8L194 150.2 47.9 171.4c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.1 23 46 46.4 33.7L288 439.6V0z"
+                  ></path>
+                </svg>
+              </span>
+            </span>
+            <p>{customers[index].text}</p>
+            <span className="QuoteIcon">
+              <svg
+                class="svg-inline--fa fa-quote-left fa-w-16"
+                aria-hidden="true"
+                data-prefix="fas"
+                data-icon="quote-left"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                data-fa-i2svg=""
+              >
+                <path
+                  fill="currentColor"
+                  d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"
+                ></path>
+              </svg>
+            </span>
+            <button className="LeftBtn" onClick={leftBtnHandler}>
+              <svg
+                class="svg-inline--fa fa-chevron-left fa-w-10"
+                aria-hidden="true"
+                data-prefix="fas"
+                data-icon="chevron-left"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 320 512"
+                data-fa-i2svg=""
+              >
+                <path
+                  fill="currentColor"
+                  d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"
+                ></path>
+              </svg>
+            </button>
+            <button className="RightBtn" onClick={rightBtnHandler}>
+              <svg
+                class="svg-inline--fa fa-chevron-right fa-w-10"
+                aria-hidden="true"
+                data-prefix="fas"
+                data-icon="chevron-right"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 320 512"
+                data-fa-i2svg=""
+              >
+                <path
+                  fill="currentColor"
+                  d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
+                ></path>
+              </svg>
+            </button>
           </div>
-          <button className="rightBtn"></button>
         </div>
       </div>
     </div>
