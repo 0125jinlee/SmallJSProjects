7697b822af3ea92d6b0af250fee73870cad4fe41
Jin Won Lee <0125jinwonlee@gmail.com>
2021-02-01 22:11:22 -0800

    11. Tip Form Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDollarSign, faUser } from "@fortawesome/free-solid-svg-icons";
 
 import "./App.css";
 
 const App = () => {
+  const [billAmt, setBillAmt] = useState(0);
+  const [people, setPeople] = useState(0);
+  const [service, setService] = useState(0);
+  const [tipAmt, setTipAmt] = useState(0);
+  const [notification, setNotification] = useState(false);
+  const [calculateBtn, setCalculateBtn] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setTipAmt(billAmt * service);
+  }, [billAmt, service]);
+
+  const billAmtHandler = (e) => {
+    e.preventDefault();
+    setBillAmt(Number(e.target.value));
+  };
+
+  const setPeopleHandler = (e) => {
+    e.preventDefault();
+    setPeople(Number(e.target.value));
+  };
+
+  const setServiceHandler = (e) => {
+    e.preventDefault();
+    setService(Number(e.target.value));
+  };
+
+  const setCalculateBtnHandler = (e) => {
+    e.preventDefault();
+    if (billAmt === 0 || people === 0 || service === 0) {
+      setCalculateBtn(false);
+      setNotification(true);
+      setTimeout(() => setNotification(false), 4000);
+    } else if (
+      !calculateBtn &&
+      billAmt !== 0 &&
+      people !== 0 &&
+      service !== 0
+    ) {
+      setLoading(true);
+      setTimeout(() => setLoading(false), 2000);
+      setTimeout(() => setCalculateBtn(true), 2000);
+    }
+  };
+
   return (
     <div className="App">
       <div className="Container">
@@ -13,11 +58,15 @@ const App = () => {
             <h2>TIP CALCULATOR</h2>
           </div>
           <div className="Body">
-            <div className="Notification">
-              <p>Bill amount cannot be blank</p>
-              <p>Number of users must be greater than zero</p>
-              <p>You must select a Service</p>
-            </div>
+            {notification ? (
+              <div className="Notification">
+                {billAmt === 0 ? <p>Bill amount cannot be blank</p> : null}
+                {people === 0 ? (
+                  <p>Number of users must be greater than zero</p>
+                ) : null}
+                {service === 0 ? <p>You must select a Service</p> : null}
+              </div>
+            ) : null}
             <form className="TipForm">
               <section>
                 <h3>How Much Was Your Bill ?</h3>
@@ -30,7 +79,11 @@ const App = () => {
                       />
                     </span>
                   </div>
-                  <input type="number" className="NumberInput" />
+                  <input
+                    type="number"
+                    className="NumberInput"
+                    onChange={(e) => billAmtHandler(e)}
+                  />
                 </div>
               </section>
               <section>
@@ -41,46 +94,58 @@ const App = () => {
                       <FontAwesomeIcon className="UserIcon" icon={faUser} />
                     </span>
                   </div>
-                  <input type="number" className="NumberInput" />
+                  <input
+                    type="number"
+                    className="NumberInput"
+                    onChange={(e) => setPeopleHandler(e)}
+                  />
                 </div>
               </section>
               <section>
                 <h3>How Was Your Service?</h3>
                 <div className="TipList">
-                  <select>
+                  <select onChange={(e) => setServiceHandler(e)}>
                     <option selected value="0">
                       Choose...
                     </option>
-                    <option value="1">Great - 20%</option>
-                    <option value="2">Good - 15%</option>
-                    <option value="3">Okay - 10%</option>
-                    <option value="4">Bad - 1%</option>
+                    <option value=".20">Great - 20%</option>
+                    <option value=".15">Good - 15%</option>
+                    <option value=".10">Okay - 10%</option>
+                    <option value=".01">Bad - 1%</option>
                   </select>
                 </div>
                 <input
                   className="CalculateButton"
                   type="submit"
                   value="Calculate"
+                  onClick={setCalculateBtnHandler}
                 />
               </section>
             </form>
-            <div className="Loading">
-              <img src="/loading.gif" alt="Loading" />
-            </div>
-            <div className="Result">
-              <h3>
-                Tip Amount
-                <span>$</span> <span></span>
-              </h3>
-              <h3>
-                Total Amount
-                <span>$</span> <span></span>
-              </h3>
-              <h3>
-                Each Person Owes
-                <span>$</span> <span></span>
-              </h3>
-            </div>
+            {loading ? (
+              <div className="Loading">
+                <img src="Loading.gif" alt="Loading" />
+              </div>
+            ) : null}
+            {calculateBtn ? (
+              <div className="Result">
+                <h3>
+                  Tip Amount
+                  <span> ${parseFloat(tipAmt).toFixed(2)}</span>
+                </h3>
+                <h3>
+                  Total Amount
+                  <span> ${parseFloat(billAmt + tipAmt).toFixed(2)}</span>
+                </h3>
+                <h3>
+                  Each Person Owes
+                  <span>
+                    {" "}
+                    ${parseFloat((billAmt + tipAmt) / people).toFixed(2)}
+                  </span>
+                </h3>
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
